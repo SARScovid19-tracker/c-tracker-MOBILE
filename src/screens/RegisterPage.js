5021154ebff22c9a1333f82d0c39c066cbe83e39
Wisma Eka <wismaeka21@gmail.com>
2020-10-17 13:03:36 +0800

    - added skeleton to login and register screen - installed react-native components - setting stack navigation app.js - on progress in app nitification (sandbox.js)

--- src/screens/RegisterPage.js
+++ src/screens/RegisterPage.js
@@ -1,9 +1,97 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { styles } from '../styles/styles'
+import { View, Image, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native'
 
-export default function RegisterPage () {
+
+export default function RegisterPage() {
+
+  const styles = StyleSheet.create({
+    container: {
+      paddingTop: 5,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    img: {
+      height: 200,
+      width: 200
+    },
+    imgNormal: {
+      paddingTop: 0,
+      height: 200,
+      width: 200
+    },
+    inputField: {
+      width: 280,
+      color: 'white',
+      borderColor: 'white',
+      marginTop: 5
+    },
+    Wrapper: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      // backgroundColor: '#1F3A93'
+    },
+    text: {
+      color: 'blue',
+      fontSize: 23
+    }
+  });
+
   return (
-    <Text>RegisterPage</Text>
+    <>
+
+      <View style={styles.container}>
+        <Image style={styles.img}
+          source={require('../../assets/logo-removebg-preview.png')}
+        />
+      </View>
+      <View
+        behavior="padding"
+        style={styles.Wrapper}>
+        <Text>
+          Name
+      </Text>
+        <TextInput
+          underlineColorAndroid='black'
+          placeholderTextColor='black'
+          keyboardType='email-address'
+          style={styles.inputField} />
+        <Text>
+          Email
+      </Text>
+        <TextInput
+          underlineColorAndroid='black'
+          placeholderTextColor='black'
+          keyboardType='email-address'
+          style={styles.inputField} />
+
+        <Text>
+          Phone Number
+      </Text>
+        <TextInput
+          underlineColorAndroid='black'
+          placeholderTextColor='black'
+          keyboardType='email-address'
+          style={styles.inputField} />
+
+        <Text>
+          First Six Digit NIK
+      </Text>
+        <TextInput
+          underlineColorAndroid='black'
+          placeholderTextColor='black'
+          // coba cari set max nya 6
+          keyboardType='number'
+          style={styles.inputField} />
+
+
+        <View >
+          <Button title="Register" mode="outlined" dark={true}>
+            {/* onPress={() => toRegister()} */}
+            {/* berhasil ke login , kalo belum muncul validasi */}
+          </Button>
+        </View>
+      </View>
+    </>
   )
 }
