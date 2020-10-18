8213186d95e9b7a694bce9a3f96d358654278955
Wisma Eka <wismaeka21@gmail.com>
2020-10-18 15:09:11 +0800

    - added action for login - refactor registerpage

---
+++ src/actions/actions.js
@@ -0,0 +1,4 @@
+import axios from 'axios'
+import { USER_LOGIN, USER_LOGOUT } from './action-type'
+
+
--- src/screens/RegisterPage.js
+++ src/screens/RegisterPage.js
@@ -9,7 +9,7 @@ export default function RegisterPage({ navigation }) {
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [phone, setphone] = useState(0);
+  const [phone, setphone] = useState('');
   const [nik, setNik] = useState(0);
 
   const [expoPushToken, setExpoPushToken] = useState('');
@@ -46,94 +46,94 @@ export default function RegisterPage({ navigation }) {
         deviceId: expoPushToken
       }
       axios
-      .post('https://068537906b97.ngrok.io/register',
-      qs.stringify(data), {
-        headers: { 'content-type': 'application/x-www-form-urlencoded' }
-      })
-      .then( function(res) {
-        console.log(`RESP: ${res.data}`)
-      })
-      .catch( function(error){
-        console.log(`ERR: ${error}`)
-      })
-    // navigation.navigate('LoginPage')
-  } else {
-    if (name === '') {
-      Alert.alert('Name must be filled in!')
-    } else if (email === '') {
-      Alert.alert('Email must be filled in!')
-    } else if (phone === '') {
-      Alert.alert('phone must be filled in!')
-    } else if (nik > 7 || nik < 7) {
-      Alert.alert('Input your first six number NIK')
-    } else if (!expoPushToken) {
-      Alert.alert('Use a reliable device')
+        .post('https://068537906b97.ngrok.io/register',
+          qs.stringify(data), {
+          headers: { 'content-type': 'application/x-www-form-urlencoded' }
+        })
+        .then(function (res) {
+          console.log(`RESP: ${res.data}`)
+        })
+        .catch(function (error) {
+          console.log(`ERR: ${error}`)
+        })
+      navigation.navigate('LoginPage')
+    } else {
+      if (name === '') {
+        Alert.alert('Name must be filled in!')
+      } else if (email === '') {
+        Alert.alert('Email must be filled in!')
+      } else if (phone === '') {
+        Alert.alert('phone must be filled in!')
+      } else if (nik > 7 || nik < 7 || nik === '') {
+        Alert.alert('Input your first six number NIK')
+      } else if (!expoPushToken) {
+        Alert.alert('Use a reliable device')
+      }
     }
+
   }
 
-}
-
-return (
-  <>
-    <View style={styles.container}>
-      <Image style={styles.img}
-        source={require('../../assets/logo-removebg-preview.png')}
-      />
-    </View>
-    <View
-      behavior="padding"
-      style={styles.Wrapper}>
-      {/* <Text>Your expo push token: {expoPushToken}</Text> */}
-      <Text>
-        Name
-      </Text>
-      <TextInput
-        onChangeText={(text) => setName(text)}
-        underlineColorAndroid='black'
-        placeholderTextColor='black'
-        keyboardType='email-address'
-        style={styles.inputField} />
-      <Text>
-        Email
-      </Text>
-      <TextInput
-        onChangeText={(text) => setEmail(text)}
-        underlineColorAndroid='black'
-        placeholderTextColor='black'
-        keyboardType='email-address'
-        style={styles.inputField} />
-
-      <Text>
-        Phone
-      </Text>
-      <TextInput
-        onChangeText={(num) => setphone(num)}
-        underlineColorAndroid='black'
-        placeholderTextColor='black'
-        keyboardType='number-pad'
-        style={styles.inputField} />
-
-      <Text>
-        First Six Digit NIK
-      </Text>
-      <TextInput
-        onChangeText={(num) => setNik(num)}
-        underlineColorAndroid='black'
-        placeholderTextColor='black'
-        // coba cari set max nya 6
-        keyboardType='number-pad'
-        style={styles.inputField} />
-
-
-      <View >
-        <Button onPress={(event) => submitRegister(event)} title="Register" mode="outlined" dark={true}>
-
-          {/* berhasil ke login , kalo belum muncul validasi */}
-        </Button>
+  return (
+    <>
+      <View style={styles.container}>
+        <Image style={styles.img}
+          source={require('../../assets/logo-removebg-preview.png')}
+        />
       </View>
-    </View>
-  </>
-)
+      <View
+        behavior="padding"
+        style={styles.Wrapper}>
+        {/* <Text>Your expo push token: {expoPushToken}</Text> */}
+        <Text>
+          Name
+      </Text>
+        <TextInput
+          onChangeText={(text) => setName(text)}
+          underlineColorAndroid='black'
+          placeholderTextColor='black'
+          keyboardType='email-address'
+          style={styles.inputField} />
+        <Text>
+          Email
+      </Text>
+        <TextInput
+          onChangeText={(text) => setEmail(text)}
+          underlineColorAndroid='black'
+          placeholderTextColor='black'
+          keyboardType='email-address'
+          style={styles.inputField} />
+
+        <Text>
+          Phone
+      </Text>
+        <TextInput
+          onChangeText={(num) => setphone(num)}
+          underlineColorAndroid='black'
+          placeholderTextColor='black'
+          keyboardType='number-pad'
+          style={styles.inputField} />
+
+        <Text>
+          First Six Digit NIK
+      </Text>
+        <TextInput
+          onChangeText={(num) => setNik(num)}
+          underlineColorAndroid='black'
+          placeholderTextColor='black'
+          // coba cari set max nya 6
+          keyboardType='number-pad'
+          style={styles.inputField} />
+
+
+        <View >
+          <Button onPress={(event) => submitRegister(event)} title="Register" mode="outlined" dark={true}>
+
+            {/* berhasil ke login , kalo belum muncul validasi */}
+          </Button>
+        </View>
+      </View>
+    </>
+  )
 }
 
 async function registerForPushNotificationsAsync() {
